48358cfcd8d7befc4eebfbc4ad45c5640940a7ab
Mateo <marami26@eafit.edu.co>
2021-04-29 18:19:45 -0500

    Connected with DB

--- app.js
+++ app.js
@@ -2,45 +2,58 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({
+  region: 'us-east-2',
+  apiVersion: '2012-08-10',
+});
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-function llamadoDistancia() {
-  return Math.random() * (1000 - 10) + 10;
-}
-
-function llamadoTiempo(transporte) {
-  let tiempo = Math.random() * 100;
-  if (transporte == 'Aereo') {
-    tiempo = tiempo / 100;
-  } else if (transporte == 'Maritimo') {
-    tiempo = tiempo / 10;
-  }
-  return Math.floor(tiempo);
-}
-
-app.post('/transporte', cors(), (req, res) => {
-  body = req.body;
-  distanciaRecorrido = llamadoDistancia(
-    body.origen,
-    body.destino,
-    body.transporteTipo
-  );
-  tiempoRecorrido = llamadoTiempo(
-    body.origen,
-    body.destino,
-    body.transporteTipo
-  );
-  transporteUsado = body.transporteTipo;
-  res.send({
-    distanciaRecorrido: `${distanciaRecorrido} km`,
-    tiempoRecorrido: `${tiempoRecorrido} s`,
-    transporteUsado,
-  });
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post('/transporte', cors(), async (req, res) => {
+  try {
+    const { transporte } = req.body;
+    const transportes = getTransportesMapper(await getTransportes());
+    const distanciaRecorrido = Math.round(Math.random() * (1000 - 10) + 10);
+    res.send({
+      ['Distancia (Km)']: distanciaRecorrido,
+      ['Tiempo (Min)']: calcularTiempo(transporte, transportes),
+    });
+  } catch (e) {
+    res.status(500).send({
+      error: e.message || 'Something went wrong',
+    });
+  }
+});
+
+function calcularTiempo(transporte, transportes) {
+  console.log(transportes[transporte]);
+  if (!transportes[transporte]) throw new Error('Transporte no soportado');
+  return Math.round(
+    (Math.random() * (10000 - 100) + 100) / transportes[transporte]
+  );
+}
+
+const getTransportes = () => {
+  const params = {
+    TableName: 'everGreenTransporte',
+  };
+  return new Promise((resolve, reject) => {
+    dynamodb.scan(params, function (err, data) {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+};
+
+function getTransportesMapper(transportes) {
+  return transportes.Items.map(({ tipo, velocidad }) => ({
+    [tipo.S]: velocidad.N,
+  })).reduce((acc, cur) => Object.assign(acc, cur), {});
+}
